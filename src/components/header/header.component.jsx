aab3cb86d79a9aff5e18625b8e27f80f01a0d4e3
pdw63 <paul1.wells@gmail.com>
2020-05-27 17:58:09 +0100

    aded redux and added reducer and action to header and app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
@@ -13,11 +14,15 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>SHOP</Link>
             <Link className='option' to='/contact'>CONTACT</Link>
             {currentUser ?
-            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>}
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                :
+                <Link className='option' to='/signin'>SIGN IN</Link>}
         </div>
     </div>
 )
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -46,7 +46,7 @@ class SignIn extends React.Component {
                     <FormInput name="password" type="password" value={this.state.password} label="password" required handleChange={this.handleChange} />
                     <div className="buttons">
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
                             Sign in with Google
                         </CustomButton>
                     </div>
