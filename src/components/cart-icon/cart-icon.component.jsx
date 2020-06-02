6262c81d4517c9eee4f1897971ea00362304b9b4
pdw63 <paul1.wells@gmail.com>
2020-06-02 15:01:32 +0100

    add items to cart and group using quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,15 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppin-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
+const CartIcon = ({toggleCartHiddenProp}) => (
+    <div className='cart-icon' onClick={toggleCartHiddenProp}>
         <ShoppingIcon className='shopping-icon' />
         <span className='item-count'>0</span>
     </div>
 )
 
 const mapDispatcherToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+    toggleCartHiddenProp: () => dispatch(toggleCartHidden())
 })
 
 export default connect(null, mapDispatcherToProps) (CartIcon);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        >
+const CollectionItem = ({ item, addItemProp }) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            >
+            </div>
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>${price}</span>
+            </div>
+            <CustomButton onClick={() => addItemProp(item)} inverted>Add to cart</CustomButton>
         </div>
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>${price}</span>
-        </div>
-    </div>
-);
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItemProp: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
