6dadff2d0f71efe90129cc9cf5462b31028de489
pdw63 <paul1.wells@gmail.com>
2020-06-09 16:25:07 +0100

    use reselect to optimise cart renders

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppin-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHiddenProp}) => (
+const CartIcon = ({ toggleCartHiddenProp, itemCountProp }) => (
     <div className='cart-icon' onClick={toggleCartHiddenProp}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCountProp}</span>
     </div>
 )
 
@@ -15,4 +16,8 @@ const mapDispatcherToProps = dispatch => ({
     toggleCartHiddenProp: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatcherToProps) (CartIcon);
+const mapStateToProps = state => ({
+    itemCountProp: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatcherToProps)(CartIcon);
